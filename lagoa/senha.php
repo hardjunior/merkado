c33b4ba5a9fca4d17cc2e409e91e42970f7aa355
Ivamar Junior <hardjunior1@gmail.com>
2020-08-15 00:58:43 +0100

    Adicionar pagina lagoa

---
+++ lagoa/senha.php
@@ -0,0 +1,6 @@
+<?php
+if (basename($_SERVER["PHP_SELF"]) == "senha.php") {
+	die("<center ><h1>Este arquivo não pode ser acessado diretamente.");
+}
+   $usuario   = 'automatico@merkadorestaurante.pt';    // SMTP username
+   $Password   = '2020merkado';                        // SMTP username
